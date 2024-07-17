443b73ed944e7c5fc19efc1d848e2db205ddaa7e
Aditya Gupta <aditya.gupta_chd@yahoo.ca>
2024-07-16 17:19:22 -0700

    Added links to navbar

--- frontend/roulettech-assigment/src/components/navbar/Navbar.js
+++ frontend/roulettech-assigment/src/components/navbar/Navbar.js
@@ -1,10 +1,21 @@
+import { Link } from 'react-router-dom'
+
 export default function Navbar() {
   return <header>
     <div>Aditya Gupta</div>
     <nav>
       <ul>
         <li>
-          
+          <Link to='/'>Profile</Link>
+        </li>
+        <li>
+          <Link to='/experience'>Experience</Link>
+        </li>
+        <li>
+          <Link to='/projects'>Projects</Link>
+        </li>
+        <li>
+          <Link to='/contact-me'>Contact Me</Link>
         </li>
       </ul>
     </nav>
