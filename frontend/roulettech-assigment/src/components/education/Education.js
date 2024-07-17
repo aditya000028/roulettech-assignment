2861cd20cc122c86d4012ec48698e26cb7aa6f56
Aditya Gupta <aditya.gupta_chd@yahoo.ca>
2024-07-17 13:32:41 -0700

    added background pic for education

--- frontend/roulettech-assigment/src/components/education/Education.js
+++ frontend/roulettech-assigment/src/components/education/Education.js
@@ -3,14 +3,16 @@ import classes from "./Education.module.css";
 export default function Education() {
   return (
     <div className={classes.education}>
-      <div className={classes.text}>
-        <h1>Education</h1>
-        <p>
-          ugywe weg uygef uweg fywegf uyewg fuyweg fuyweg fuyg uyrg fyuerig
-          hriuhg oidrut ygoery goiureh goiuwhreoiugywroeiu gyoiwuryg iouwrey
-          goiuerhghfk ghsdkhf hgliueh giuerhiugherlui ghwlieurh rgliuwerh
-          glihewrlg elruihg liueh gliqerh liuqehrgoiuhqergiueh r
-        </p>
+      <div className={classes.content}>
+        <div className={classes.text}>
+          <h1>Education</h1>
+          <p>
+            ugywe weg uygef uweg fywegf uyewg fuyweg fuyweg fuyg uyrg fyuerig
+            hriuhg oidrut ygoery goiureh goiuwhreoiugywroeiu gyoiwuryg iouwrey
+            goiuerhghfk ghsdkhf hgliueh giuerhiugherlui ghwlieurh rgliuwerh
+            glihewrlg elruihg liueh gliqerh liuqehrgoiuhqergiueh r
+          </p>
+        </div>
       </div>
     </div>
   );
