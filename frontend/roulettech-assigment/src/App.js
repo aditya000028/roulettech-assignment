9fb55d470279fe0231e71297e94b749266c2f67b
Aditya Gupta <aditya.gupta_chd@yahoo.ca>
2024-07-16 17:16:38 -0700

    Added basic pages

--- frontend/roulettech-assigment/src/App.js
+++ frontend/roulettech-assigment/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+
+import ProfilePage from "./pages/Profile";
+import ExperiencePage from "./pages/Experience";
+import ProjectsPage from "./pages/Projects";
+import ContactMePage from "./pages/Contact-Me";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path="/" element={<ProfilePage />} />
+        <Route path="/experience" element={<ExperiencePage />} />
+        <Route path="/projects" element={<ProjectsPage />} />
+        <Route path="/contact-me" element={<ContactMePage />} />
+      </Routes>
     </div>
   );
 }
---
+++ frontend/roulettech-assigment/src/components/navbar/Navbar.js
@@ -0,0 +1,12 @@
+export default function Navbar() {
+  return <header>
+    <div>Aditya Gupta</div>
+    <nav>
+      <ul>
+        <li>
+          
+        </li>
+      </ul>
+    </nav>
+  </header>
+}
