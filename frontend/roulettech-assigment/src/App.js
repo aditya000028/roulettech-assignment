868e2b683f3ebde75eee90a672ff1ca89a0d1c3d
Aditya Gupta <aditya.gupta_chd@yahoo.ca>
2024-07-17 22:43:13 -0700

    removed pages

--- frontend/roulettech-assigment/src/App.js
+++ frontend/roulettech-assigment/src/App.js
@@ -1,19 +1,11 @@
-import { Routes, Route } from "react-router-dom";
-
 import ProfilePage from "./pages/profile/Profile";
-import ExperiencePage from "./pages/experience/Experience";
-import ProjectsPage from "./pages/projects/Projects";
 import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
     <div>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<ProfilePage />} />
-        <Route path="/experience" element={<ExperiencePage />} />
-        <Route path="/projects" element={<ProjectsPage />} />
-      </Routes>
+      <ProfilePage />
     </div>
   );
 }
