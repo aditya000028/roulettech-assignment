34a6c654f55f833c73d8f5544891f34546625b29
Aditya Gupta <aditya.gupta_chd@yahoo.ca>
2024-07-18 14:01:44 -0700

    Added comment model and initial migration

--- backend/comments/models.py
+++ backend/comments/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Comment(models.Model):
+  firstName = models.CharField(max_length=128, blank=False)
+  lastName = models.CharField(max_length=128, blank=False)
+  timestamp = models.DateTimeField(blank=False)
+  subject = models.CharField(max_length=128, blank=False)
+  comment = models.TextField(max_length=256, blank=False)
