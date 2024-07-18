11d83ac21632a4094ea124e9f3ce351304c36689
Aditya Gupta <aditya.gupta_chd@yahoo.ca>
2024-07-17 23:01:33 -0700

    Added icons to navbar

--- frontend/roulettech-assigment/src/components/navbar/Navbar.js
+++ frontend/roulettech-assigment/src/components/navbar/Navbar.js
@@ -1,25 +1,59 @@
-import { Link } from "react-router-dom";
 import profilePicture from "../../pictures/ProfilePicture.jpeg";
-import { Avatar } from "@mui/material"
+import { Avatar } from "@mui/material";
 import classes from "./Navbar.module.css";
+import { AiFillLinkedin, AiFillGithub, AiFillPhone } from "react-icons/ai";
+import { MdEmail } from "react-icons/md";
+import { IoGlobeOutline } from "react-icons/io5";
 
 export default function Navbar() {
   return (
     <header>
       <div className={classes.aditya}>
-        <Avatar alt="profile" src={profilePicture} className={classes.profilePicture} />
+        <Avatar
+          alt="profile"
+          src={profilePicture}
+          className={classes.profilePicture}
+        />
         <h1>Aditya Gupta</h1>
       </div>
       <nav>
         <ul className={classes.links}>
           <li>
-            <Link to="/">Profile</Link>
+            <a
+              href="https://www.linkedin.com/in/aditya-gupta-developer/"
+              target="_blank"
+              rel="noreferrer author"
+            >
+              <AiFillLinkedin className={classes.icon} />
+            </a>
           </li>
           <li>
-            <Link to="/experience">Experience</Link>
+            <a
+              href="https://www.portofaditya.com/"
+              target="_blank"
+              rel="noreferrer author"
+            >
+              <IoGlobeOutline className={classes.icon} />
+            </a>
           </li>
           <li>
-            <Link to="/projects">Projects</Link>
+            <a
+              href="https://github.com/aditya000028"
+              target="_blank"
+              rel="noreferrer author"
+            >
+              <AiFillGithub className={classes.icon} />
+            </a>
+          </li>
+          <li>
+            <a href="mailto:aditya.g000028@gmail.com">
+              <MdEmail className={classes.icon} />
+            </a>
+          </li>
+          <li>
+            <a href="tel:+14255243452">
+              <AiFillPhone className={classes.icon} />
+            </a>
           </li>
         </ul>
       </nav>
