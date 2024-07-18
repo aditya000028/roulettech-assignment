cd691ac1e6108cbd669fb5e3e4d8fdf5add1df1b
Aditya Gupta <aditya.gupta_chd@yahoo.ca>
2024-07-17 23:25:28 -0700

    Fixed css bug

--- frontend/roulettech-assigment/src/components/introduction/Introduction.js
+++ frontend/roulettech-assigment/src/components/introduction/Introduction.js
@@ -4,16 +4,18 @@ import profilePicture from "../../pictures/ProfilePicture.jpeg";
 export default function Introduction() {
   return (
     <div className={classes.introduction}>
-      <div className={classes.text}>
-        <h1>Aditya Gupta</h1>
-        <h2>Software Engineer</h2>
-        <p>
-          jh bwkuhbewbwe fukwegi fweugyf uyeg fyuwegf uweg fuygew fuygwe fuweg
-          fwgyef owgef kuagek ufgakf gwaeiuf iwe fkagdsfkuywge fiuwEKUYFG wkeug
-          fkuwg fkugeywf kuywegf kuweg fkugy
-        </p>
+      <div className={classes.content}>
+        <div className={classes.text}>
+          <h1>Aditya Gupta</h1>
+          <h2>Software Engineer</h2>
+          <p>
+            jh bwkuhbewbwe fukwegi fweugyf uyeg fyuwegf uweg fuygew fuygwe fuweg
+            fwgyef owgef kuagek ufgakf gwaeiuf iwe fkagdsfkuywge fiuwEKUYFG
+            wkeug fkuwg fkugeywf kuywegf kuweg fkugy
+          </p>
+        </div>
+        <img src={profilePicture} alt="profile" />
       </div>
-      <img src={profilePicture} alt="profile" />
     </div>
   );
 }
